87df89a49222ff8242fbec8b475b3044f9e5658c
Sadhasivam S <62686520+Ishidaa-Asahi@users.noreply.github.com>
2024-06-30 19:45:38 +0530

    initial commit with a basic project

---
+++ src/test/java/com/tinkerwizard/devopsdemo/DevopsdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tinkerwizard.devopsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
